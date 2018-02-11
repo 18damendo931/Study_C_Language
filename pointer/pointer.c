d551801f19a4bc0d108cc77fe8261d27027cccef
noritoshi <ikasabi@gmail.com>
2018-02-11 23:08:00 +0900

    アドレスの表示は%pを使用するように変更

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -13,15 +13,15 @@ int main(void) {
 	y = 200;
 
 	printf_s("変数xの値 = %d\n", x);
-	printf_s("変数xのアドレス = %d\n", &x);
+	printf_s("変数xのアドレス = %p\n", &x);
 	printf_s("変数yの値 = %d\n", y);
-	printf_s("変数yのアドレス = %d\n", &y);
+	printf_s("変数yのアドレス = %p\n", &y);
 	printf_s("変数pxの値 = %d\n", px);
 	printf_s("変数pyの値 = %d\n", py);
 	printf_s("変数pxの参照先の値 = %d\n", *px);
 	printf_s("変数pyの参照先の値 = %d\n", *py);
-	printf_s("変数pxのアドレス = %d\n", &px);
-	printf_s("変数pyのアドレス = %d\n", &py);
+	printf_s("変数pxのアドレス = %p\n", &px);
+	printf_s("変数pyのアドレス = %p\n", &py);
 
 	return 0;
 }
